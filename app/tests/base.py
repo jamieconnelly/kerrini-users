a5a8093ff78554be2eab53a358b88227607edd14
jamieconnelly <jamiebruceconnelly@gmail.com>
2017-11-12 15:36:43 +0100

    :white_checkmark: setup tests

---
+++ app/tests/base.py
@@ -0,0 +1,17 @@
+from app import app, db
+
+from flask_testing import TestCase
+
+
+class BaseTestCase(TestCase):
+    def create_app(self):
+        app.config.from_object('app.config.TestingConfig')
+        return app
+
+    def setUp(self):
+        db.create_all()
+        db.session.commit()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
--- manage.py
+++ manage.py
@@ -1,8 +1,20 @@
+import unittest
+
 from app import app, db
 
 from flask_script import Manager
 
 manager = Manager(app)
+
+
+@manager.command
+def test():
+    """Runs the tests without code coverage."""
+    tests = unittest.TestLoader().discover('app/tests', pattern='test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        return 0
+    return 1
 
 
 @manager.command
